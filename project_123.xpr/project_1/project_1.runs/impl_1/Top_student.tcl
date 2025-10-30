7bb12166ecab543b1e07be12e9e1b90680bc4368
XuZihao <xuzihao024572@gmail.com>
2025-10-31 03:39:41 +0800

    add bullet skill introduction

--- project_123.xpr/project_1/project_1.runs/impl_1/Top_student.tcl
+++ project_123.xpr/project_1/project_1.runs/impl_1/Top_student.tcl
@@ -69,12 +69,12 @@ set rc [catch {
   create_project -in_memory -part xc7a35tcpg236-1
   set_property design_mode GateLvl [current_fileset]
   set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir D:/github/EE2026-Star-War/project_123.xpr/project_1/project_1.cache/wt [current_project]
-  set_property parent.project_path D:/github/EE2026-Star-War/project_123.xpr/project_1/project_1.xpr [current_project]
-  set_property ip_output_repo D:/github/EE2026-Star-War/project_123.xpr/project_1/project_1.cache/ip [current_project]
+  set_property webtalk.parent_dir C:/Users/XuZihao/Documents/GitHub/EE2026-Star-War/project_123.xpr/project_1/project_1.cache/wt [current_project]
+  set_property parent.project_path C:/Users/XuZihao/Documents/GitHub/EE2026-Star-War/project_123.xpr/project_1/project_1.xpr [current_project]
+  set_property ip_output_repo C:/Users/XuZihao/Documents/GitHub/EE2026-Star-War/project_123.xpr/project_1/project_1.cache/ip [current_project]
   set_property ip_cache_permissions {read write} [current_project]
-  add_files -quiet D:/github/EE2026-Star-War/project_123.xpr/project_1/project_1.runs/synth_1/Top_student.dcp
-  read_xdc D:/github/EE2026-Star-War/project_123.xpr/project_1/project_1.srcs/constrs_1/new/my_constriant.xdc
+  add_files -quiet C:/Users/XuZihao/Documents/GitHub/EE2026-Star-War/project_123.xpr/project_1/project_1.runs/synth_1/Top_student.dcp
+  read_xdc C:/Users/XuZihao/Documents/GitHub/EE2026-Star-War/project_123.xpr/project_1/project_1.srcs/constrs_1/new/my_constriant.xdc
   link_design -top Top_student -part xc7a35tcpg236-1
   close_msg_db -file init_design.pb
 } RESULT]
